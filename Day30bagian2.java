e0f5d0143bb07d5390c39d936b195b0b5cf9267c
aldiahayy <aldisalam0756@gmail.com>
2025-10-07 22:59:07 +0800

    Day30bagian2.java

--- Day30bagian2.java
+++ Day30bagian2.java
@@ -1,7 +1,17 @@
 package enamOkt;
 
 import java.util.Scanner;
-
+/*Seorang petani tua meninggalkan sejumlah harta warisan untuk ketiga anaknya setelah wafat. Harta tersebut terdiri dari sejumlah emas dalam bentuk batangan yang harus dibagi sesuai permintaan khusus petani tersebut. Petani ingin:
+· Anak pertama mendapatkan setengah dari total emas.
+· Anak kedua mendapatkan sepertiga dari total emas yang tersisa setelah anak pertama mengambil bagiannya.
+· Anak ketiga mendapatkan sisanya.
+Tugas Anda adalah menentukan berapa jumlah emas yang didapatkan oleh
+masing-masing anak. Jumlah total emas akan diberikan sebagai input, dan Anda harus menghitung pembagian emas sesuai dengan aturan di atas.
+Format Input :
+Sebuah bilangan bulat positif T yang merupakan jumlah total emas (dalam batangan).
+Format Output :
+Cetak tiga bilangan bulat yang masing-masing menyatakan jumlah emas
+yang diterima oleh anak pertama, anak kedua, dan anak ketiga secara berurutan.*/
 public class soal2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
