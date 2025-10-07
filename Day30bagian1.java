65ca2256ed0f946d58b735895c18994205da961f
aldiahayy <aldisalam0756@gmail.com>
2025-10-07 23:00:09 +0800

    Day30bagian1.java

--- Day30bagian1.java
+++ Day30bagian1.java
@@ -1,7 +1,16 @@
 package enamOkt;
 
 import java.util.Scanner;
-
+/*Anda diberikan sebuah papan persegi panjang berukuran M × N. Anda juga diberikan sejumlah tak terbatas kartu ceki standar berukuran 2 × 1. Anda diizinkan untuk memutar kartu-kartu tersebut. Anda diminta untuk menyusun sebanyak mungkin ceki di papan dengan syarat berikut:
+1. Setiap ceki menutupi dua kotak secara penuh.
+2. Tidak ada dua ceki yang saling tumpang tindih.
+3. Setiap ceki harus sepenuhnya berada di dalam papan. ceki boleh menyentuh tepi papan.
+Tentukan jumlah maksimum ceki yang dapat disusun sesuai dengan batasan batasan ini.
+Format Input :
+Dalam satu baris, Anda diberikan dua bilangan bulat M dan N (ukuran papan dalam
+kotak)
+Format Output :
+Keluaran berupa satu angka (jumlah maksimum ceki yang dapat disusun).*/
 public class soal1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
