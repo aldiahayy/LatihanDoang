4dd13704cf7908aa0e6b0cf952362d9dd8572968
aldiahayy <aldisalam0756@gmail.com>
2025-10-07 22:49:28 +0800

    Day30bagian3.java

---
+++ Day30bagian3.java
@@ -0,0 +1,15 @@
+package enamOkt;
+
+import java.util.Scanner;
+
+public class soal3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int p = sc.nextInt();
+        int m = sc.nextInt();
+        
+        System.out.println(p/m);
+        System.out.println(p%m);
+    }
+    
+}
