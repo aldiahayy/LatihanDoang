96b6c1623b134c23f11ef94ad2b0bda95ababdc7
aldiahayy <aldisalam0756@gmail.com>
2025-10-07 22:57:54 +0800

    Day30bagian3.java

--- Day30bagian3.java
+++ Day30bagian3.java
@@ -1,7 +1,13 @@
 package enamOkt;
 
 import java.util.Scanner;
-
+/*Seorang guru sedang membagikan sejumlah permen kepada murid-muridnya. Setiap murid harus mendapatkan jumlah permen yang sama, dan tidak boleh ada sisa permen setelah semua permen dibagikan. Diberikan sejumlah permen P dan jumlah murid M, hitunglah berapa banyak permen yang diterima setiap murid, dan berapa banyak permen yang tersisa jika ada.
+Format Input :
+Baris pertama berisi sebuah bilangan bulat positif P yang menunjukkan jumlah total permen.
+Baris kedua berisi sebuah bilangan bulat positif M yang menunjukkan jumlah murid.
+Format Output :
+Baris pertama berisi sebuah bilangan bulat positif yang menunjukkan jumlah permen yang diterima oleh setiap murid.
+Baris kedua berisi sebuah bilangan bulat positif yang menunjukkan sisa permen yang tidak terbagi.*/
 public class soal3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
