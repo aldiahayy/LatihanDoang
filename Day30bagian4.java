687fe4f176755760a32330bef235369d07700462
aldiahayy <aldisalam0756@gmail.com>
2025-10-07 22:54:47 +0800

    Day30bagian4.java

--- Day30bagian4.java
+++ Day30bagian4.java
@@ -1,6 +1,18 @@
 package enamOkt;
 
 import java.util.Scanner;
+/*Seorang petani memiliki dua ladang dengan ukuran berbeda. Ladang
+pertama memiliki panjang P1 meter dan lebar L1 meter, sedangkan
+ladang kedua memiliki panjang P2 meter dan lebar L2 meter. Petani
+ingin mengetahui selisih luas antara kedua ladang tersebut.
+Bantulah petani untuk menghitung selisih luas antara ladang
+pertama dan ladang kedua
+Format Input :
+Dua integer P1 dan L1 untuk panjang dan lebar ladang pertama.
+Dua integer P2 dan L2 untuk panjang dan lebar ladang kedua.
+Format Output :
+Satu integer yang merupakan selisih luas antara kedua ladang.
+gunakan Math.abs()*/
 
 public class soal4 {
     public static void main(String[] args) {
