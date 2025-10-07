3f00d736d5b532954e42c4411577a91de62d8308
aldiahayy <aldisalam0756@gmail.com>
2025-10-07 22:53:04 +0800

    Day30bagian5.java

--- Day30bagian5.java
+++ Day30bagian5.java
@@ -1,6 +1,26 @@
 package enamOkt;
 
 import java.util.Scanner;
+/*Seorang arsitek sedang menghitung biaya pembangunan rumah dengan
+beberapa variabel biaya yang harus dihitung ulang. Kamu diminta untuk membantu menghitung total biaya
+pembangunan rumah tersebut dengan beberapa operasi sederhana.
+Arsitek memiliki tiga biaya awal:
+· biayaBahan: biaya bahan bangunan.
+· biayaPekerja: biaya upah para pekerja.
+· biayaPerizinan: biaya izin pembangunan.
+Setelah dilakukan perhitungan ulang, arsitek ingin memperbarui nilai
+dari biaya bahan dan biaya pekerja menggunakan aturan berikut:
+· biayaBahan akan dinaikkan sebesar 20%.
+· biayaPekerja akan dinaikkan sebesar 15%.
+Setelah itu, kamu diminta menghitung total biaya akhir yang merupakan
+penjumlahan dari ketiga biaya tersebut setelah diperbarui.
+Format Input :
+Baris pertama berisi bilangan bulat biayaBahan
+Baris kedua berisi bilangan bulat biayaPekerja
+Baris ketiga berisi bilangan bulat biayaPerizinan
+Format Output :
+Cetak satu bilangan bulat yang merupakan total biaya pembangunan
+setelah penyesuaian.*/
 
 public class soal5 {
     public static void main(String[] args) {
