621de775fe6f42c534078f2e4d845473deb74bfd
aldi <aldisalam0756@gmail.com>
2026-01-04 17:18:09 +0800

    no8.java
    Deklarasikan sekaligus inisialisasi 2 array dengan tipe data berbeda (float dan char) kemudian tampilkan outputnya!
    
    Contoh output:
    Array float:
    1.5 2.3 3.7 4.2 5.9 6.2 7.3 
    
    Array char:
    B E L A J A R

--- soalAsdosDDp/no8.java
+++ soalAsdosDDp/no8.java
@@ -1,11 +1,3 @@
-/*Deklarasikan sekaligus inisialisasi 2 array dengan tipe data berbeda (float dan char) kemudian tampilkan outputnya!
-
-Contoh output:
-Array float:
-1.5 2.3 3.7 4.2 5.9 6.2 7.3 
-
-Array char:
-B E L A J A R*/
 public class no8 {
     public static void main(String[] args) {
         float f [] = {1.5f,2.3f,3.7f,4.2f,5.9f,6.2f,7.3f};
