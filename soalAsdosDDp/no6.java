44e1692095d213c8bbc4a5962a97c3f577588320
aldi <aldisalam0756@gmail.com>
2026-01-04 17:24:43 +0800

    soal 6
    Buatlah sebuah program Java yang menggunakan array untuk menyimpan minimal 10 nama mahasiswa.
    Program harus menampilkan seluruh nama yang ada di dalam array.
    kemudian meminta pengguna untuk memasukkan sebuah nama yang ingin dicari.
    Setelah pengguna memasukkan nama tersebut, program akan melakukan pencarian untuk menemukan
    posisi nama tersebut di dalam array. Jika ditemukan, program akan menampilkan pesan berupa nama
    yang dicari beserta indeks ke-berapa nama tersebut berada. Pencarian nama tidak bersifat case-sensitive.
    
    Menampilkan isi array terlebih dahulu
    Nama :  rifky rajab daud rendi hanip parma azisah hasnur ali ade
    Input : 
    cari : hanip
    Output : 
    rajab berada pada index ke-4
    
    Input : 
    cari : daud
    Output : daud
    rajab berada pada index ke-2

--- soalAsdosDDp/no6.java
+++ soalAsdosDDp/no6.java
@@ -1,21 +1,3 @@
-/*Buatlah sebuah program Java yang menggunakan array untuk menyimpan minimal 10 nama mahasiswa.
-Program harus menampilkan seluruh nama yang ada di dalam array.
-kemudian meminta pengguna untuk memasukkan sebuah nama yang ingin dicari.
-Setelah pengguna memasukkan nama tersebut, program akan melakukan pencarian untuk menemukan
-posisi nama tersebut di dalam array. Jika ditemukan, program akan menampilkan pesan berupa nama
-yang dicari beserta indeks ke-berapa nama tersebut berada. Pencarian nama tidak bersifat case-sensitive.
-
-Menampilkan isi array terlebih dahulu
-Nama :  rifky rajab daud rendi hanip parma azisah hasnur ali ade
-Input : 
-cari : hanip
-Output : 
-rajab berada pada index ke-4
-
-Input : 
-cari : daud
-Output : daud
-rajab berada pada index ke-2*/
 import java.util.Scanner;
 public class no6 {
     public static void main(String[] args) {
