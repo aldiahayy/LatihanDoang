6a5f4bfd1365d347654f137f1d89c605c121e0d6
aldi <aldisalam0756@gmail.com>
2026-01-06 20:32:44 +0800

    soal 12
    buat program yang meminta pengguna menginputkan tahun lahirnya lalu menampilkan umur pengguna saat ini
    contoh input:
    Masukkan Tahun lahir: 2007
    output:
    Umur anda saat ini adalah 19 tahun

---
+++ soalAsdosDDp/no12.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class no12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukan tahun lahir: ");
+        int tahun = sc.nextInt();
+        System.out.println("Umur anda saat ini adalah " +(2026-tahun) + " tahun");
+    }
+}
