4dd14e02150dad2c5c867120e9cd5ccc9055a0c4
aldi <aldisalam0756@gmail.com>
2026-01-28 13:22:02 +0800

    soal 23
    buat blok kode (method/fungsi) untuk menghitung nilai keliling persegi (K = 4 x sisi) dengan panjang setiap sisinya itu adalah 8cm (nilai panjang setiap sisi harus di masukkan diparameter).
    
    output:
    32 cm.

--- soalAsdosDDp/no23.java
+++ soalAsdosDDp/no23.java
@@ -1,5 +1,18 @@
-public class Main {
-	public static void main(String[] args) {
-		System.out.println("halo");
-	}
+public class no23 {
+    public static void main(String[] args) {
+        System.out.println(kelilingPersegi1(8)+" cm");
+        kelilingPersegi2(8);
+        
+        no23 no23 = new no23();
+        System.out.println(no23.kelilingPersegi3(8)+" cm");
+    }
+    public static int kelilingPersegi1(int sisi){
+     return 4*sisi;   
+    }
+    public static void kelilingPersegi2(int sisi){
+        System.out.println((4*sisi)+" cm");
+    }
+    int kelilingPersegi3(int sisi){
+        return 4*sisi;
+    }
 }
