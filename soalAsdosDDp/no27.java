69b3a0cc0dd9e001522fd097b1382bc006644fc6
aldi <aldisalam0756@gmail.com>
2026-01-31 10:04:42 +0800

    soal 27

--- soalAsdosDDp/no27.java
+++ soalAsdosDDp/no27.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class no28 {
+public class no27 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Masukan angka: ");
