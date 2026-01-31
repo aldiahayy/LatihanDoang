a687c650a93593a8af7ba8f6064096e1e4b3de55
aldi <aldisalam0756@gmail.com>
2026-01-31 10:06:44 +0800

    soal 29
    Program Penilaian Kelulusan
    Lulus jika nilai teori lebih atau sama dengan 70 dan nilai praktek lebih atau sama dengan 80. Tidak lulus jika salah satu nilai tidak memenuhi syarat. Buat program Java yang menerima dua input nilai (teori dan praktek), lalu menampilkan:
    
    Input: 
    Nilai teori : 70
    Nilai praktek : 80
    
    Output : 
    Selamat, Anda lulus
    
    Input: 
    Nilai teori : 69
    Nilai praktek : 79
    
    Output : 
    maaf, Anda belum lulus

--- soalAsdosDDp/no29.java
+++ soalAsdosDDp/no29.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class no30 {
+public class no29 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Masukan nilai teori: ");
