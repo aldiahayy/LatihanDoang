22eccfc7d56822ddf2a1a6dc0c91b994cfdb6d54
aldi <aldisalam0756@gmail.com>
2026-01-31 10:06:13 +0800

    soal 28
    Sebuah perpustakaan mencatat jumlah total buku yang dipinjam. Rak buku hanya mampu menampung 8 buku per rak. Hitunglah jumlah rak minimal yang dibutuhkan untuk menampung semua buku, dan sisa buku yang tidak tertampung di rak terakhir.
    
    Contoh input:
    Total buku yang dipinjam: 20
    
    Output:
    Total buku: 20
    Rak yang dibutuhkan: 3
    Sisa buku: 4

--- soalAsdosDDp/no28.java
+++ soalAsdosDDp/no28.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class no29 {
+public class no28 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Masukan total buku yang dipinjam: ");
