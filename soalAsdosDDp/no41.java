383263dbce44f2dd3cd5b838188fe02c447ffaa8
aldi <aldisalam0756@gmail.com>
2026-02-07 14:19:51 +0800

    soal 41
    Buat program yang meminta input sebuah angka, lalu menampilkan apakah angka tersebut merupakan bilangan bulat atau bilangan desimal.
    Catatan: Bilangan 0 tidak termasuk ke dalam bilangan bulat maupun desimal.
    
    contoh input 1 : 
    Masukkan angka : 6
    
    output : 
    Bilangan bulat
    
    contoh input 2 :
    Masukkan angka : 2.9
    
    output :
    Bilangan desimal

--- soalAsdosDDp/no41.java
+++ soalAsdosDDp/no41.java
@@ -1,5 +1,17 @@
-public class Main {
-	public static void main(String[] args) {
-		System.out.println("halo");
-	}
+import java.util.Scanner;
+public class no41 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukan angka: ");
+        double angka = sc.nextDouble();
+        String deteksi = "";
+        if (angka == 0) {
+            deteksi = "ini adalah angka nol";
+        }else if (angka % 1 == 0) {
+            deteksi = "Bilangan bulat";
+        }else{
+            deteksi = "Bilangan desimal";
+        }
+        System.out.println(deteksi);
+    }
 }
