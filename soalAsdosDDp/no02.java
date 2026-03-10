99dd590cbd12a1a573333a8a36d922207a4e19c1
aldi <aldisalam0756@gmail.com>
2026-03-10 15:01:35 +0800

    soal 02
    buat sebuah program untuk menampung nilai array sebanyak 10 dari pengguna kemudian tampilkan hanya angka genap di atas 50.
    contoh input:
    10
    20
    30
    40
    50
    51
    52
    53
    54
    55
    
    output:
    52
    54

--- soalAsdosDDp/no02.java
+++ soalAsdosDDp/no02.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class no2 {
+public class no02 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int nilai[] = new int[10];
