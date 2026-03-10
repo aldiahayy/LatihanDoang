f7516905d853935690752fce430a19d67664c40c
aldi <aldisalam0756@gmail.com>
2026-03-10 15:04:55 +0800

    soal 03
    Buat sebuah program yang menerima 5 angka sebagai input.
    Program kemudian menentukan dan menampilkan angka terbesar dan angka terkecil.
    
    contoh input :
    4 10 3 8 6
    
    output :
    Angka terbesar: 10
    Angka terkecil: 3

--- soalAsdosDDp/no03.java
+++ soalAsdosDDp/no03.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class no3 {
+public class no03 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int nilai [] = new int[5];
