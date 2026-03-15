92c34a7384f018ad62db25be95d480b8e9725632
aldi <aldisalam0756@gmail.com>
2026-03-15 11:10:09 +0800

    soal 02

---
+++ DDpFarming/soal02.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class soal02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukan batas(n): ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i += 2) {
+            System.out.print(i + " ");
+        }
+    }
+}
