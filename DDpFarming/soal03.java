d726ec42c7805553d9d127d5ad95aedf6b200bb9
aldi <aldisalam0756@gmail.com>
2026-03-15 11:15:49 +0800

    soal03

---
+++ DDpFarming/soal03.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class soal03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukan harga: ");
+        int harga = sc.nextInt();
+        double diskon;
+        double total = harga;
+        if (harga  == 100000) {
+            diskon = harga * 0.1;
+            total = harga - diskon;
+        } else if (harga == 500000) {
+            diskon = harga * 0.2;
+            total = harga - diskon;
+        } else {
+            System.out.println("tidak dapat diskon");
+        }
+        System.out.println("Total harga  : " + total);
+    }
+}
