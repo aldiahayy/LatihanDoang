3f99656f249cd6d5da16315928d9f6da4abeddd5
aldi <aldisalam0756@gmail.com>
2026-03-21 08:29:45 +0800

    soal 05
    Anda diberikan sebuah papan persegi panjang berukuran M × N. Anda juga diberikan sejumlah tak terbatas kartu ceki standar berukuran 2 × 1. Anda diizinkan untuk memutar kartu-kartu tersebut. Anda diminta untuk menyusun sebanyak mungkin ceki di papan dengan syarat berikut:
    1. Setiap ceki menutupi dua kotak secara penuh.
    2. Tidak ada dua ceki yang saling tumpang tindih.
    3. Setiap ceki harus sepenuhnya berada di dalam papan. ceki boleh menyentuh tepi papan.
    Tentukan jumlah maksimum ceki yang dapat disusun sesuai dengan batasan batasan ini.
    Format Input :
    Dalam satu baris, Anda diberikan dua bilangan bulat M dan N (ukuran papan dalam
    kotak)
    Format Output :
    Keluaran berupa satu angka (jumlah maksimum ceki yang dapat disusun).

---
+++ LatihanISC/soal05.java
@@ -0,0 +1,12 @@
+package LatihanISC;
+import java.util.Scanner;
+public class soal05 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int c = 2*1;
+        
+        System.out.println(m*n/c);           
+    }   
+}
