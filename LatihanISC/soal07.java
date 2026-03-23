c7a3c35236eb0823efa37c47d4fe8ac6f9d637af
aldi <aldisalam0756@gmail.com>
2026-03-23 13:48:45 +0800

    soal 07
    Seorang guru sedang membagikan sejumlah permen kepada murid-muridnya. Setiap murid harus mendapatkan jumlah permen yang sama, dan tidak boleh ada sisa permen setelah semua permen dibagikan. Diberikan sejumlah permen P dan jumlah murid M, hitunglah berapa banyak permen yang diterima setiap murid, dan berapa banyak permen yang tersisa jika ada.
    Format Input :
    Baris pertama berisi sebuah bilangan bulat positif P yang menunjukkan jumlah total permen.
    Baris kedua berisi sebuah bilangan bulat positif M yang menunjukkan jumlah murid.
    Format Output :
    Baris pertama berisi sebuah bilangan bulat positif yang menunjukkan jumlah permen yang diterima oleh setiap murid.
    Baris kedua berisi sebuah bilangan bulat positif yang menunjukkan sisa permen yang tidak terbagi.

---
+++ LatihanISC/soal07.java
@@ -0,0 +1,12 @@
+package LatihanISC;
+import java.util.Scanner;
+public class soal07 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int p = sc.nextInt();
+        int m = sc.nextInt();
+        
+        System.out.println(p/m);
+        System.out.println(p%m);
+    }
+}
