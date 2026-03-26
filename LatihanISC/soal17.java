c84d028c143baeeeabb87e00d4120d03b293fc9b
aldi <aldisalam0756@gmail.com>
2026-03-27 06:35:15 +0800

    soal 17
    Buat program yang menyimpan dan menampilkan biodata: nama, umur, dan kota asal. Semua disimpan di variabel terlebih dahulu, lalu dicetak ke layar.

---
+++ LatihanISC/soal17.java
@@ -0,0 +1,11 @@
+package LatihanISC;
+public class soal17 {
+    public static void main(String[] args) {
+        String nama = "Aldi Salam";
+        int umur = 19;
+        String kota = "Mamuju";
+        System.out.println(nama);
+        System.out.println(umur);
+        System.out.println(kota);
+    }
+}
