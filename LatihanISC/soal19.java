54dd4516b76fe013b0b2da78a17d484e7e549c10
aldi <aldisalam0756@gmail.com>
2026-03-28 20:04:08 +0800

    soal 19
    Kalkulator Aritmatika Dasar
    Buat program yang:
    • meminta input dua bilangan bulat;
    • menampilkan hasil: penjumlahan, pengurangan, perkalian, dan pembagian (integer).
    Tampilkan dengan format rapi, misalnya: a + b = ....

---
+++ LatihanISC/soal19.java
@@ -0,0 +1,13 @@
+package LatihanISC;
+import java.util.Scanner;
+public class soal19 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("penjumlahan: " + (a + b));
+        System.out.println("pengurangan: " + (a - b));
+        System.out.println("perkalian  : " + (a * b));
+        System.out.println("pembagian  : " + (a / b));
+    }
+}
